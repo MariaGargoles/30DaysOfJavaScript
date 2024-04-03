a0a82bfb0d45ccc1ed447e231678202a90f74855
María Gárgoles <mariagargoles@live.com>
2024-04-03 16:05:13 +0200

    ADD DAY1

---
+++ day1.js
@@ -0,0 +1,33 @@
+
+let strVariable = "This is a string";
+let boolVariable = true;
+let undefinedVariable = undefined;
+let nullVariable = null;
+
+
+console.log(typeof strVariable); 
+console.log(typeof boolVariable); 
+console.log(typeof undefinedVariable); 
+console.log(typeof nullVariable); 
+
+
+let var1, var2, var3, var4;
+
+
+let var5 = "value1", var6 = true, var7 = null, var8 = 10;
+
+
+let firstName = "John";
+let lastName = "Doe";
+let maritalStatus = "Single";
+let country = "USA";
+let age = 30;
+
+
+let firstNameSingleLine = "John", lastNameSingleLine = "Doe", maritalStatusSingleLine = "Single", countrySingleLine = "USA", ageSingleLine = 30;
+
+
+let myAge = 25;
+let yourAge = 30;
+console.log("My age is:", myAge);
+console.log("Your age is:", yourAge);
