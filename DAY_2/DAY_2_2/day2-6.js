7121f86c2343118317a500f6d5bdae2022e4acac
María Gárgoles <mariagargoles@live.com>
2024-04-04 19:34:43 +0200

    ADD 2-6 ex

---
+++ DAY_2/DAY_2_2/day2-6.js
@@ -0,0 +1,7 @@
+//I hope this course is not full of jargon. Check if jargon is in the sentence.
+
+let str = "I hope this course is not full of jargon.";
+
+const checkstr = str.includes("jargon");
+
+console.log(checkstr);
