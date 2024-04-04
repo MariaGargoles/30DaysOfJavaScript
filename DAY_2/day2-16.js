463f2dea57f085c243df883474a515c341634fcb
María Gárgoles <mariagargoles@live.com>
2024-04-04 17:06:21 +0200

    ADD ex16

---
+++ DAY_2/day2-16.js
@@ -0,0 +1,6 @@
+//Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+
+
+let str ="30 Days Of JavaScript";
+const occA =str.indexOf('a');
+console.log(occA);
