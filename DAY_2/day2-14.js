f689de80981a3e8947d8ccb78cccab9339baa722
María Gárgoles <mariagargoles@live.com>
2024-04-04 17:03:01 +0200

    ADD ex15

--- DAY_2/day2-14.js
+++ DAY_2/day2-14.js
@@ -1,9 +1,6 @@
 //What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
 let str = "30 Days Of JavaScript";
 
-const strCharJ = str.charCodeAt(str.indexOf('J'));
-
-console.log(strCharJ);
-
+console.log(str.charCodeAt(11)); 
 
 
---
+++ DAY_2/day2-15.js
@@ -0,0 +1,10 @@
+//Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+
+let str ="30 Days Of JavaScript";
+const occA =str.charCodeAt(str.indexOf('a'));
+console.log(occA);
+
+
+//Comprobacion con 
+console.log(str.charCodeAt(4)); 
+
