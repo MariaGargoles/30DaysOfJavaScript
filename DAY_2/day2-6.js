8625a455402329c3fb2d3cd3cdec00331850503a
María Gárgoles <mariagargoles@live.com>
2024-04-04 15:38:50 +0200

    end ex6

--- DAY_2/day2-6.js
+++ DAY_2/day2-6.js
@@ -1,1 +1,5 @@
 //Cut (slice) out the first word of the string using substr() or substring() method
+
+const str ="Hellow world"
+
+console.log(str.substring(1));
---
+++ day2-7.js
@@ -0,0 +1,2 @@
+//Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+
