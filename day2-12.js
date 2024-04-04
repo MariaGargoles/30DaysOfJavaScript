9cecab075c84aa413eea3fee1ce966cb24726125
María Gárgoles <mariagargoles@live.com>
2024-04-04 16:40:35 +0200

    add ex13

---
+++ day2-12.js
@@ -0,0 +1,4 @@
+//Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+let str = "30 Days Of JavaScript";
+let strReplace = str.replace("JavaScript", "Python");
+console.log(strReplace);
---
+++ day2-13.js
@@ -0,0 +1,6 @@
+//What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+
+let str = "30 Days Of JavaScript";
+let strChar = str.charAt(15);
+
+console.log(strChar);
