fb3f82012dc8cc5e982ce5e0a0d7f9530f7c0538
María Gárgoles <mariagargoles@live.com>
2024-04-04 17:28:05 +0200

    ADD 2-2-1 EX

---
+++ DAY_2/DAY_2_1/day2-25.js
@@ -0,0 +1,5 @@
+//Use repeat() method to print 30 Days Of JavaScript 2 times
+
+const str = "30 Days Of JavaScript";
+const repeatedString = str.repeat(2);
+console.log(repeatedString); 
---
+++ DAY_2/DAY_2_2/day2-1.js
@@ -0,0 +1,3 @@
+//Using console.log() print out the following statement:
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.");
