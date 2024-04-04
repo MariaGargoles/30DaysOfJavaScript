8516e810902381e362b895290f5df54feb504f6f
María Gárgoles <mariagargoles@live.com>
2024-04-04 16:54:05 +0200

    ADD ex14

---
+++ DAY_2/day2-14.js
@@ -0,0 +1,9 @@
+//What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+let str = "30 Days Of JavaScript";
+
+const strCharJ = str.charCodeAt(str.indexOf('J'));
+
+console.log(strCharJ);
+
+
+
