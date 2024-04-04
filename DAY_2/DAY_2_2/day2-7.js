090cdcd85d7a1e1e0e834b857749015dd3b420e2
María Gárgoles <mariagargoles@live.com>
2024-04-04 19:44:43 +0200

    ADD 2-7 EX

---
+++ DAY_2/DAY_2_2/day2-7.js
@@ -0,0 +1,8 @@
+//Generate a random number between 0 and 100 inclusively.
+const randomNum = Math.random()* 101;
+console.log(randomNum);
+
+//añadimos math.floor para que no salga en decimales
+
+const randomNum1 = Math.floor(Math.random() * 101);
+console.log(randomNum1);
