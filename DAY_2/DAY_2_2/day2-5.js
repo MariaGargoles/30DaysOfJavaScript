50f7dcad371f43fb9d0a4d48855a40fcf2bc142d
María Gárgoles <mariagargoles@live.com>
2024-04-04 19:32:04 +0200

    ADD 2-5 EX

---
+++ DAY_2/DAY_2_2/day2-5.js
@@ -0,0 +1,9 @@
+//Check if 'on' is found in both python and jargon
+
+let str1 = "python";
+let str2 = "jargon";
+
+const checkstr1 = str1.includes('on');
+const checkstr2 = str2.includes('on');
+
+console.log(checkstr1, checkstr2);
