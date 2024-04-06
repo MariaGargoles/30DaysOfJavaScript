078499a5cd83599a6a33a5c133c6306a0b05a11b
María Gárgoles <mariagargoles@live.com>
2024-04-06 18:04:33 +0200

    ADD 3-2-10

---
+++ DAY_3/DAY_3_2/day3-10.js
@@ -0,0 +1,9 @@
+//If the length of your name is greater than 7 say, your name is long else say your name is short.
+
+let name = prompt("Indica tu nombre: ");
+
+if (name.length >= 7) {
+    console.log("Tu nombre es largo");
+} else {
+    console.log("Tu nombre es corto");
+}
