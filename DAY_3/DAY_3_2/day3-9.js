af82618bbd466b77e029b679130558b5b655c4e8
María Gárgoles <mariagargoles@live.com>
2024-04-06 17:52:54 +0200

    ADD 3-2-9 no realuzo del 5 al 8 porque son matematicos puros

---
+++ DAY_3/DAY_3_2/day3-9.js
@@ -0,0 +1,6 @@
+//Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
+
+let hours = prompt("Introduce tus horas semanales: ");
+let rate = prompt("Introduce tu rate la hora: ");
+
+console.log("Tu salario semanal es:", (hours * rate));
