863f29a07f0483dff2496171a9213ae86d6dd71e
María Gárgoles <mariagargoles@live.com>
2024-04-06 17:17:14 +0200

    Fix days. ADD day 3-2-1

---
+++ DAY_3/DAY_3_2/day3-1.js
@@ -0,0 +1,13 @@
+/*Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
+
+Enter base: 20
+Enter height: 10
+The area of the triangle is 100
+
+*/
+
+let base = prompt("Introducir base: ");
+let ancho = prompt("Introducir ancho: ");
+const a = 0.5;
+
+console.log(a*base*ancho);
