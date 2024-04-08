7b7c317f394f6e13f0f85d1fcc6c68ae8108e15b
María Gárgoles <mariagargoles@live.com>
2024-04-08 10:54:52 +0200

    day 3-2-12

---
+++ DAY_3/DAY_3_2/day3-12.js
@@ -0,0 +1,18 @@
+//Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
+/*
+let myAge = 250
+let yourAge = 25
+
+I am 225 years older than you.
+*/
+
+let myAge = 250
+let yourAge = 25
+
+let compAges = myAge - yourAge
+
+if (myAge>yourAge) {
+    console.log("I am", compAges, " years older than you.");
+}else{
+    console.log("You are", compAges," years older than me");
+}
