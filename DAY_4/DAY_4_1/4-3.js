93a4eba50d431c055bab2e9e589af5db59190a5c
María Gárgoles <mariagargoles@live.com>
2024-04-11 19:33:27 +0200

    add 4-1-3

---
+++ DAY_4/DAY_4_1/4-3.js
@@ -0,0 +1,15 @@
+/*If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
+
+using if else
+ternary operator.
+
+let a = 4
+let b = 3
+
+4 is greater than 3*/
+
+let a = 4;
+let b = 3;
+
+let result = (a > b) ? 'a is greater than b' : 'a is less than b';
+console.log(result);
