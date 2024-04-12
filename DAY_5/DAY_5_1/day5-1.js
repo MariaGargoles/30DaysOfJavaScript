9e7a51d0a8215c34d82eb19efbe6f9c2164844e4
María Gárgoles <mariagargoles@live.com>
2024-04-12 15:56:14 +0200

    ADD

---
+++ DAY_5/DAY_5_1/day5-1.js
@@ -0,0 +1,41 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+  
+  const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB'
+  ]
+
+ /*Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+Print the array using console.log()*/ 
+ 
+
+/*Declare an empty array;*/
+  const empyarray = [];
+
+// Declare an array with more than 5 number of elements
+  const numarray = ['5','6','7','8','9'];
+
+  //Find the length of your array 
+  console.log(numarray.length)
+// Get the first item, the middle item and the last item of the array
+  console.log(numarray[0])
+  console.log(numarray[2])
+  console.log(numarray[4])
