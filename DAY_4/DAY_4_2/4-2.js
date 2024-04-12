74f25acc465952ecdbe7a5e3a5c0bc2e128c6918
María Gárgoles <mariagargoles@live.com>
2024-04-12 10:05:08 +0200

    Exploramos la opcion de convertir los meses en array para su posterior comparacion

---
+++ DAY_4/DAY_4_2/4-2.js
@@ -0,0 +1,11 @@
+/*Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+
+September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer*/
+
+let spring = ['March', 'April', 'May'];
+console.log(typeof spring);
+let springever = spring.includes("March");
+console.log(springever);
