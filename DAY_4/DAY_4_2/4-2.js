6ddc9b43edaddc008e6a030570e7598af45aac68
María Gárgoles <mariagargoles@live.com>
2024-04-12 10:43:03 +0200

    Add ex 4-2-4

--- DAY_4/DAY_4_2/4-2.js
+++ DAY_4/DAY_4_2/4-2.js
@@ -5,7 +5,23 @@ December, January or February, the season is Winter.
 March, April or May, the season is Spring
 June, July or August, the season is Summer*/
 
-let spring = ['March', 'April', 'May'];
-console.log(typeof spring);
-let springever = spring.includes("March");
-console.log(springever);
+let spring = ['Marzo', 'Abril', 'Mayo'];
+let summer = ['Junio', 'Julio', 'Agosto'];
+let autumn = ['Septiembre', 'Octubre', 'Noviembre'];
+let winter = ['Diciembre', 'Enero', 'Febrero'];
+
+let season = prompt("En que mes del año estamos: ")
+
+if (spring.includes(season)){
+    console.log("Es primavera")
+} else if (summer.includes(season)){
+    console.log("Es verano")
+}else if (autumn.includes(season)){
+    console.log("Es Otoño")
+}else if (winter.includes(season)){
+    console.log("Es Invierno")
+}else{
+    console.log("Incorrecto")
+}    
+
+
