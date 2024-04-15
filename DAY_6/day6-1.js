553bfe39de47cf7567b001448270f841d2bcc330
María Gárgoles <mariagargoles@live.com>
2024-04-15 10:05:44 +0200

    add 6 ex 6,1,2,3,4,5

---
+++ DAY_6/day6-1.js
@@ -0,0 +1,55 @@
+/*Exercises: Level 1
+
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+1.Iterate 0 to 10 using for loop, do the same using while and do while loop
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']*/
+
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+//Loop 
+for (let i = 0; i < countries.length; i++) {
+  const element = countries[i];
+}
+
+//2. Iterate 10 to 0 using for loop, do the same using while and do while loop
+
+let i = 0
+do {
+  console.log(i)
+  i++
+} while (i <= countries.length)
+
